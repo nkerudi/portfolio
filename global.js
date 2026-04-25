4b4c09c89ee8d4b3a62d308adc31dadfe40e67ae
nkerudi <nikithakerudi@gmail.com>
2026-04-24 18:52:55 -0700

    lab 4 changes, added js styling

--- global.js
+++ global.js
@@ -93,3 +93,44 @@ form?.addEventListener("submit", (event) => {
   url += params.join("&");
   location.href = url;
 });
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+    
+  }
+}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  if (!containerElement) {
+    console.error('Projects container not found.');
+    return;
+  }
+  containerElement.innerHTML = '';
+  if (!projects || projects.length === 0) {
+    containerElement.textContent = 'No projects found.';
+    return;
+  }
+  for (const project of projects) {
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+  }
+}
+
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
