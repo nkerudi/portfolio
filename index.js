326b8813289ab4dd537dd5057a567e510b27962b
nkerudi <nikithakerudi@gmail.com>
2026-04-25 17:15:02 -0700

    Fixed homepage Javascript imports

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-import { fetchJSON, renderProjects, fetchGithubData } from "./global";
-const projects = await fetchJSON('./lib/projects.json');
+import { fetchJSON, renderProjects, fetchGitHubData } from "./global.js";
+
+const projects = await fetchJSON("./lib/projects.json");
 const latestProjects = projects.slice(0, 3);
-const projectsContainer = document.querySelector('.projects');
-renderProjects(latestProjects, projectsContainer, 'h2');
-const githubData = await fetchGithubData('nkerudi');
-const profileStats = document.querySelector('#profile-stats');
-if (profileStats){
-    profileStats.innerHTML = `
+
+const projectsContainer = document.querySelector(".projects");
+renderProjects(latestProjects, projectsContainer, "h2");
+
+const githubData = await fetchGitHubData("nkerudi");
+const profileStats = document.querySelector("#profile-stats");
+
+if (profileStats) {
+  profileStats.innerHTML = `
     <dl>
       <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
       <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
@@ -14,7 +18,6 @@ if (profileStats){
       <dt>Following:</dt><dd>${githubData.following}</dd>
     </dl>
   `;
-
 }
 
 
