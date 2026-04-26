954e9f0369f52cc2f91e9a65bf58bb9b3d68c1db
nkerudi <nikithakerudi@gmail.com>
2026-04-25 17:28:12 -0700

    made javascript fixes

--- global.js
+++ global.js
@@ -4,10 +4,7 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const BASE_PATH =
-  location.hostname === "localhost" || location.hostname === "127.0.0.1"
-    ? "/"
-    : "/portfolio/";
+const BASE_PATH = "/portfolio/";
 
 const pages = [
   { url: "", title: "Home" },
@@ -96,16 +93,17 @@ form?.addEventListener("submit", (event) => {
 
 export async function fetchJSON(url) {
   try {
-    // Fetch the JSON file from the given URL
     const response = await fetch(url);
+
     if (!response.ok) {
       throw new Error(`Failed to fetch projects: ${response.statusText}`);
     }
+
     const data = await response.json();
     return data;
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
-    
+    return [];
   }
 }
 
